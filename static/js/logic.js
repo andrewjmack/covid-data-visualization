30dc7a76593b43e8a4ad2fdb3e935a3a59d17c54
tarsian <jacob.kim.kh@gmail.com>
2024-06-06 20:07:11 -0500

    .
    changed

--- static/js/logic.js
+++ static/js/logic.js
@@ -48,30 +48,28 @@ function resetHighlight(e) {
     geojson.resetStyle(e.target);
 }
 
-function showInfoAndZoom(e) {
-  var layer = e.target;
-  var stateName = layer.feature.properties.name;
-
-  // Update the info panel header
-  document.getElementById('info-panel-header').innerHTML = '<h2>' + stateName + '</h2>';
-
-  // Fit the map to the clicked feature's bounds
-  map.fitBounds(layer.getBounds());
-}
-
 // Define a function to show info and zoom on click
-/*
 function showInfoAndZoom(e) {
     var layer = e.target;
     var stateName = layer.feature.properties.name;
 
+    // Update the info panel header
+    document.getElementById('info-panel-header').innerHTML = '<h2>' + stateName + '</h2>';
+
     // Fit the map to the clicked feature's bounds
     map.fitBounds(layer.getBounds());
-
-    // Update the info panel
-    document.getElementById('info-panel').innerHTML = '<h2>' + stateName + '</h2>';
 }
-*/
+
+function showInfoForDate() {
+    var selectedDate = document.getElementById('date-input').value;
+
+    // 예시: 선택된 날짜를 콘솔에 출력
+    console.log("Selected date:", selectedDate);
+
+    // 여기에 선택된 날짜에 해당하는 정보를 불러오는 코드를 추가
+    // fetch나 AJAX를 사용하여 서버로부터 데이터를 요청하고, 응답을 처리합니다.
+    // 응답으로 받은 데이터를 info-panel에 출력하는 등의 작업을 수행합니다.
+}
 
 // Add the GeoJSON data
 var geojson;
@@ -84,4 +82,7 @@ fetch('https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geo
             style: style,
             onEachFeature: onEachFeature
         }).addTo(map);
+
+        // Set initial state name
+        document.getElementById('info-panel-header').innerHTML = '<h2>United States</h2>';
     });
