fa25f43a6f62aac5323c5715cb6d2938c0b87c61
tarsian <jacob.kim.kh@gmail.com>
2024-06-09 18:50:31 -0500

    .

--- app/app.py
+++ app/app.py
@@ -1,6 +1,4 @@
-# Import the dependencies
-# import numpy as np
-# import datetime as dt
+# Import dependencies
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -8,6 +6,9 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+
+import pandas as pd
 
 #################################################
 # Database Setup
@@ -24,19 +25,19 @@ Base.prepare(autoload_with=engine)
 
 # Assign the epidemiology class to a variable called `Epidemiology'
 Epidemiology = Base.classes.epidemiology
+Demographic = Base.classes.demographic
+Vaccination = Base.classes.vaccination
+States = Base.classes.states
 
 # Create a session
 session = Session(bind=engine)
-
-# Calculate the date one year from the last date in data set for later use
-# most_recent = session.query(Measurement.date).\
-#     order_by(Measurement.date.desc()).first().date
-# one_year = dt.datetime.strptime(most_recent, '%Y-%m-%d') - dt.timedelta(days=365)
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
+# avoid cross origin blocking
+CORS(app)
 
 #################################################
 # Flask Routes
@@ -47,7 +48,7 @@ app = Flask(__name__)
 def welcome():
     return render_template("covidEndpoints.html")
 
-# Returns COVID epidemiology JSON data for the United States
+# Returns COVID epidemiology data for the United States
 @app.route("/api/v1.0/epidemiology_us")
 def epidemiology_us():
 
@@ -56,197 +57,148 @@ def epidemiology_us():
     print("Server received request for 'epidemiology_us' page...")
     print()
 
-    # Perform a query to retrieve the epidemiology table data
-    # epi_results = session.query(Epidemiology.location_key).all()
-    epi_results = session.query(Epidemiology.location_key,
-                                Epidemiology.date,
-                                Epidemiology.new_confirmed,
-                                Epidemiology.new_deceased,
-                                Epidemiology.new_recovered,
-                                Epidemiology.new_tested,
-                                Epidemiology.cumulative_confirmed
-                                ).all()
-    # print(epi_results)
-    
+    # create Pandas df with SQL epidemiology table
+    epi_sql_df = pd.read_sql("SELECT location_key,\
+                            date,\
+                            new_confirmed,\
+                            new_deceased,\
+                            new_recovered,\
+                            new_tested,\
+                            cumulative_confirmed,\
+                            cumulative_deceased,\
+                            cumulative_recovered,\
+                            cumulative_tested\
+                            FROM epidemiology;", con=engine)
+
+    epi_payload = list()
+
+    for idx, row in epi_sql_df.iterrows():
+        
+        epi_data = {
+            "location_key": row['location_key'],
+            "date": row['date'],
+            "new_confirmed": row['new_confirmed'],
+            "new_deceased": row['new_deceased'],
+            "new_recovered": row['new_recovered'],
+            "new_tested": row['new_tested'],
+            "cumulative_confirmed": row['cumulative_confirmed'],
+            "cumulative_deceased": row['cumulative_deceased'],
+            "cumulative_recovered": row['cumulative_recovered'],
+            "cumulative_tested": row['cumulative_tested']
+        }
+        epi_payload.append(epi_data)
+
     # close session
     session.close()
 
-    # Store results as a dictionary accessible for JSON
-    epi_dict = dict(epi_results)
+    # Return results for server call
+    return jsonify(epi_payload)
 
-    # Print results to terminal
+# Returns COVID demographic data for the United States
+@app.route("/api/v1.0/demographic_us")
+def demographic_us():
+
+    # print call acknowledgment to terminal
     print()
-    print(f"The US epidemiology results:")
+    print("Server received request for 'demographic_us' page...")
     print()
-    print(epi_dict)
-    print()
-    print(f"---- RESULTS COMPLETE ----")
-    print()
+
+    # create Pandas df with SQL demographic table
+    demo_sql_df = pd.read_sql("SELECT location_key,\
+                            population,\
+                            population_male,\
+                            population_female\
+                            FROM demographic;", con=engine)
+
+    demo_payload = list()
+
+    for idx, row in demo_sql_df.iterrows():
+        
+        demo_data = {
+            "location_key": row['location_key'],
+            "population": row['population'],
+            "population_male": row['population_male'],
+            "population_female": row['population_female']
+        }
+        demo_payload.append(demo_data)
+
+    # close session
+    session.close()
 
     # Return results for server call
-    return jsonify(epi_dict)
+    return jsonify(demo_payload)
+
+# Returns COVID vaccination data for the United States
+@app.route("/api/v1.0/vaccination_us")
+def vaccination_us():
+
+    # print call acknowledgment to terminal
+    print()
+    print("Server received request for 'vaccination_us' page...")
+    print()
+
+    # create Pandas df with SQL vaccination table
+    vac_sql_df = pd.read_sql("SELECT location_key,\
+                            date,\
+                            new_persons_vaccinated,\
+                            cumulative_persons_vaccinated,\
+                            new_persons_fully_vaccinated,\
+                            cumulative_persons_fully_vaccinated,\
+                            new_vaccine_doses_administered,\
+                            cumulative_vaccine_doses_administered\
+                            FROM demographic;", con=engine)
+
+    vac_payload = list()
+
+    for idx, row in vac_sql_df.iterrows():
+        
+        vac_data = {
+            "location_key": row['location_key'],
+            "date": row['date'],
+            "new_persons_vaccinated": row['new_persons_vaccinated'],
+            "cumulative_persons_vaccinated": row['cumulative_persons_vaccinated'],
+            "new_persons_fully_vaccinated": row['new_persons_fully_vaccinated'],
+            "cumulative_persons_fully_vaccinated": row['cumulative_persons_fully_vaccinated'],
+            "new_vaccine_doses_administered": row['new_vaccine_doses_administered'],
+            "cumulative_vaccine_doses_administered": row['cumulative_vaccine_doses_administered']
+        }
+        vac_payload.append(vac_data)
+
+    # close session
+    session.close()
+
+    # Return results for server call
+    return jsonify(vac_payload)
+
+# Returns COVID US state data for the United States
+@app.route("/api/v1.0/states")
+def states():
+
+    # print call acknowledgment to terminal
+    print()
+    print("Server received request for 'states' page...")
+    print()
+
+    # create Pandas df with SQL states table
+    states_sql_df = pd.read_sql("SELECT location_key,\
+                            state_name\
+                            FROM states;", con=engine)
+
+    states_payload = list()
+
+    for idx, row in states_sql_df.iterrows():
+        
+        states_data = {
+            "location_key": row['location_key'],
+            "state": row['state_name']
+        }
+        states_payload.append(states_data)
+
+    # close session
+    session.close()
+
+    # Return results for server call
+    return jsonify(states_payload)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-
-# Returns json with date as the key and value as the precipitation for last year in database
-# @app.route("/api/v1.0/precipitation")
-# def precipitation():
-    
-#     # print call acknowledgment to terminal
-#     print()
-#     print("Server received request for 'Precipitation' page...")
-#     print()
-
-#     # Perform a query to retrieve the date and precipitation scores.
-#     prcp_results = session.query(Measurement.date, Measurement.prcp).\
-#                                 filter(Measurement.date >= one_year).\
-#                                 order_by(Measurement.date).all()
-
-#     # close session
-#     session.close()
-
-#     # Store results as a dictionary accessible for JSON
-#     prcp_dict = dict(prcp_results)
-
-#     # Print results to terminal
-#     print()
-#     print(f"Precipitation data for the last year:")
-#     print()
-#     print(prcp_dict)
-#     print()
-#     print(f"---- RESULTS COMPLETE ----")
-#     print()
-
-#     # Return results for server call
-#     return jsonify(prcp_dict)
-
-# # Returns jsonified data of all of the stations in the database    
-# @app.route("/api/v1.0/stations")
-# def stations():
-    
-#     # print call acknowledgment to terminal
-#     print()
-#     print("Server received request for 'Stations' page...")
-#     print()
-
-#     # query station data
-#     stations = session.query(Station.station).all()
-
-#     # close session
-#     session.close()
-
-#     # store station data in accessible form for JSON
-#     station_list = list(np.ravel(stations))
-
-#     # Print results to terminal
-#     print()
-#     print(f"The list of stations:")
-#     print()
-#     print(station_list)
-#     print()
-#     print(f"---- RESULTS COMPLETE ----")
-#     print()
-
-#     # Return results for server call
-#     return jsonify(station_list)
-
-# # Returns jsonified TOBS data for the most active station (USC00519281) for the last year
-# @app.route("/api/v1.0/tobs")
-# def tobs():
-    
-#     # print call acknowledgment to terminal
-#     print()
-#     print("Server received request for 'TOBS' page...")
-#     print()
-
-#     # query temperature observations for most active station
-#     one_year_tobs = session.query(Measurement.date, Measurement.tobs).\
-#                 filter(Measurement.station == 'USC00519281').\
-#                 filter(Measurement.date > one_year).all()
-    
-#     # close session
-#     session.close()
-
-#     # store results as a dictionary accessible for JSON
-#     tobs_dict = dict(one_year_tobs)
-
-#     # Print results to terminal
-#     print()
-#     print(f"Temperature observations for station USC00519281 for the last year:")
-#     print()
-#     print(tobs_dict)
-#     print()
-#     print(f"---- RESULTS COMPLETE ----")
-#     print()
-
-#     # Return results for server call
-#     return jsonify(tobs_dict)
-
-# # Return a JSON list of the minimum temperature, the average temperature,
-# # and the maximum temperature for a specified start
-# @app.route("/api/v1.0/<start>")
-# def start(start):
-    
-#     # print call acknowledgment to terminal
-#     print()
-#     print(f"Server received request for 'TOBS' page with start date of: {start}")
-#     print()
-
-#     # ensure start from URL parameter is in datetime format (not string)
-#     start = dt.datetime.strptime(start, "%Y%m%d")
-
-#     # query data since provided start date
-#     tobs_start = session.query(func.min(Measurement.tobs),\
-#                     func.max(Measurement.tobs),\
-#                     func.avg(Measurement.tobs)).\
-#                     filter(Measurement.date >= start).all()
-    
-#     # close session
-#     session.close()
-
-#     # store station data in accessible form for JSON
-#     tobs_start_list = list(np.ravel(tobs_start))
-
-#     # Print results to terminal
-#     print()
-#     print(f"Minimum, maximum and average temperature observations since {start}:")
-#     print()
-#     print(tobs_start_list)
-#     print()
-#     print(f"---- RESULTS COMPLETE ----")
-#     print()
-
-#     # Return results for server call
-#     return jsonify(tobs_start_list)
-
-# # Return a JSON list of the minimum temperature, the average temperature,
-# # and the maximum temperature for a specified start-end range
-# @app.route("/api/v1.0/<start>/<end>")
-# def start_end(start, end):
-    
-#     # print call acknowledgment to terminal
-#     print()
-#     print(f"Server received request for 'TOBS' page with start date of: {start}")
-#     print()
-    
-#     # ensure start from URL parameter is in datetime format (not string)
-#     start = dt.datetime.strptime(start, "%Y%m%d")
-#     end = dt.datetime.strptime(end, "%Y%m%d")
-
-#     # query min, max and average TOBS from given start date to end date
-#     tobs_start_end = session.query(func.min(Measurement.tobs),\
-#                     func.max(Measurement.tobs),\
-#                     func.avg(Measurement.tobs)).\
-#                     filter(Measurement.date >= start).\
-#                     filter(Measurement.date <= end).all()
-    
-#     # close session
-#     session.close()
-
-#     # store station data in accessible form for JSON
-#     tobs_start_end_list = list(np.ravel(tobs_start_end))
-
-#     # Return results for server call
-#     return jsonify(tobs_start_end_list)
-    
--- app/schema.sql
+++ app/schema.sql
@@ -9,7 +9,7 @@
 -- Create table with primary key composed of two columns
 sqlite>
 CREATE TABLE IF NOT EXISTS epidemiology (
-	location_key VARCHAR(5) NOT NULL,
+    location_key VARCHAR(2) NOT NULL,
     date DATE  NOT NULL,
     new_confirmed INT,
     new_deceased INT,
@@ -28,8 +28,105 @@ sqlite> .schema
 
 -- Import CSV dataset from file directory
 -- “skip 1” ignores the CSV headers
-sqlite> .import --csv --skip 1 US_epi_cleaned.csv epidemiology
+sqlite> .import --csv --skip 1 ../resources/US_epi_cleaned.csv epidemiology
 
 -- Example queries to confirm successful import
 SELECT * FROM epidemiology LIMIT(5);
-SELECT * FROM epidemiology WHERE (location_key = 'US_CO') LIMIT(5);
+SELECT * FROM epidemiology WHERE (location_key = 'CO') LIMIT(5);
+
+-- Repeat steps for creation of demographic table
+sqlite>
+CREATE TABLE IF NOT EXISTS demographic (
+location_key VARCHAR(2) NOT NULL,
+population INT,
+population_male INT,
+population_female INT,
+population_age_00_09 INT,
+population_age_10_19 INT,
+population_age_20_29 INT,
+population_age_30_39 INT,
+population_age_40_49 INT,
+population_age_50_59 INT,
+population_age_60_69 INT,
+population_age_70_79 INT,
+population_age_80_and_older INT,
+PRIMARY KEY (location_key)
+);
+
+-- Import CSV dataset from file directory
+-- “skip 1” ignores the CSV headers
+sqlite> .import --csv --skip 1 ../resources/us_demo_cleaned.csv demographic
+
+-- Example queries to confirm successful import
+SELECT * FROM demographic LIMIT(5);
+SELECT * FROM demographic WHERE (location_key = 'CO') LIMIT(5);
+
+-- Repeat steps for creation of vaccination table
+sqlite>
+CREATE TABLE IF NOT EXISTS vaccination (
+location_key VARCHAR(2) NOT NULL,
+date DATE  NOT NULL,
+new_persons_vaccinated INT,
+cumulative_persons_vaccinated INT,
+new_persons_fully_vaccinated INT,
+cumulative_persons_fully_vaccinated INT,
+new_vaccine_doses_administered INT,
+cumulative_vaccine_doses_administered INT,
+new_persons_vaccinated_pfizer INT,
+cumulative_persons_vaccinated_pfizer INT,
+new_persons_fully_vaccinated_pfizer INT,
+cumulative_persons_fully_vaccinated_pfizer INT,
+new_vaccine_doses_administered_pfizer INT,
+cumulative_vaccine_doses_administered_pfizer INT,
+new_persons_vaccinated_moderna INT,
+cumulative_persons_vaccinated_moderna INT,
+new_persons_fully_vaccinated_moderna INT,
+cumulative_persons_fully_vaccinated_moderna	INT,
+new_vaccine_doses_administered_moderna INT,
+cumulative_vaccine_doses_administered_moderna INT,
+new_persons_vaccinated_janssen INT,
+cumulative_persons_vaccinated_janssen INT,
+new_persons_fully_vaccinated_janssen INT,
+cumulative_persons_fully_vaccinated_janssen	INT,
+new_vaccine_doses_administered_janssen INT,
+cumulative_vaccine_doses_administered_janssen INT,
+new_persons_vaccinated_sinovac INT,
+total_persons_vaccinated_sinovac INT,
+new_persons_fully_vaccinated_sinovac INT,
+total_persons_fully_vaccinated_sinovac INT,
+new_vaccine_doses_administered_sinovac INT,
+total_vaccine_doses_administered_sinova INT,
+PRIMARY KEY (location_key, date)
+);
+
+-- Confirm table creation and schema
+sqlite> .table
+sqlite> .schema
+
+-- Import CSV dataset from file directory
+-- “skip 1” ignores the CSV headers
+sqlite> .import --csv --skip 1 ../resources/US_vac_cleaned.csv vaccination
+
+-- Example queries to confirm successful import
+SELECT * FROM vaccination LIMIT(5);
+SELECT * FROM vaccination WHERE (location_key = 'CO') LIMIT(5);
+
+-- Repeat steps for creation of states table
+sqlite>
+CREATE TABLE IF NOT EXISTS states (
+location_key VARCHAR(2) NOT NULL,
+state_name VARCHAR(25)  NOT NULL,
+PRIMARY KEY (location_key, state_name)
+);
+
+-- Confirm table creation and schema
+sqlite> .table
+sqlite> .schema
+
+-- Import CSV dataset from file directory
+-- “skip 1” ignores the CSV headers
+sqlite> .import --csv --skip 1 ../resources/states_cleaned.csv states
+
+-- Example queries to confirm successful import
+SELECT * FROM states LIMIT(5);
+SELECT * FROM vaccination WHERE (location_key = 'CO');
